242645292e84b78862ae6a152ecd48d7ea20a0a9
twoimo <twoimo@dgu.ac.kr>
2024-10-11 17:44:39 +0900

    Refactor dotenv configuration in signup.js

--- api/signup.js
+++ api/signup.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const { Pool } = require("pg");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: ".env.development.local" });
+dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
