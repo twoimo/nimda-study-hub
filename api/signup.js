72fd06278ee4a24df65b131c13fb3fbd9750ad8c
twoimo <twoimo@dgu.ac.kr>
2024-10-12 16:04:19 +0900

    Refactor bcrypt import in login.js and signup.js to use bcrypt instead of bcryptjs

--- api/signup.js
+++ api/signup.js
@@ -1,5 +1,5 @@
 import { sql } from "@vercel/postgres";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export default async function handler(req, res) {
